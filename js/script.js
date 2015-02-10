bd425a87fd6f7d2a7fae437ffcfc95c962d54988
dennyjuice <dennyjuice@gmail.com>
2015-02-10 15:59:29 +0700

    delete onepage-scroll add work-page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($){
-	
+/*	
 	$(".main").onepage_scroll({
    sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
    easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
@@ -16,7 +16,66 @@ jQuery(document).ready(function($){
                                     // the browser's width is less than 600, the fallback will kick in.
    direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
 });
+	*/
 	
+	/*----------Gallery----------*/
+	$('.work-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Acrostia</small>';
+			}
+		}
+	});
+	/*----------END Gallery----------*/
 	
+	/*------Hover effect on img in gallery--------*/
+	$('.img-hover').hide();
+	$('.image-box').hover(function(){
+			$('.img-hover', this).show();
+		}, function(){
+			$('.img-hover', this).hide();
+		}
+	);
+	/*------END Hover effect on img in gallery--------*/
 	
+	/*------------portfolio filter--------------*/
+	$('.work-title-filter li a').click(function() {
+
+    $('.work-title-filter li').removeClass('work-filter-selected');
+    $(this).parent('li').addClass('work-filter-selected');
+		
+		var imgHeight = $('.image-box').height();
+		var thisItem    = $(this).attr('rel');
+		
+		if(thisItem !== "all") {
+ 
+    $('.image-box[rel='+thisItem+']').stop()
+                       .animate({'height' : "260px",
+                         'opacity' : 1
+                    }, 500).show();
+                                                         
+    $('.image-box[rel!='+thisItem+']').stop()
+                    .animate({'height' : 0,
+                         'opacity' : 0
+                    }, 500).hide();
+		} else {
+     
+    $('.image-box').stop()
+             .animate({'opacity' : 1,
+                 'height' : "260px"
+            }, 500).show();
+		}
+
+	});
+	/*------------END portfolio filter--------------*/
 });
